4544b7dbe4884ed49ca3bda76f4a03e0b61e1c27
Uilian Ries <uilianries@gmail.com>
2020-02-07 13:32:12 -0300

    Fix Linux build
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -11,14 +11,10 @@ class TrantorConan(ConanFile):
     description = "QuickFIX C++ Fix Engine Library"
     topics = ("HFT", "FIX", "Engine")
     settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": False, "fPIC": True}
+    options = {"shared": [True, False], "fPIC": [True, False], "ssl": [True, False], "mysql": [True, False], "postgresql": [True, False], "emx": [True, False]}
+    default_options = {"shared": False, "fPIC": True, "ssl": True, "mysql": False, "postgresql": False, "emx": False}
     generators = "cmake"
     exports_sources = "CMakeLists.txt"
-
-    requires = (
-        "openssl/1.0.2t",
-    )
 
     @property
     def _source_subfolder(self):
@@ -32,6 +28,14 @@ class TrantorConan(ConanFile):
         if self.settings.os == "Windows":
             del self.options.fPIC
 
+    def requirements(self):
+        if self.options.ssl:
+            self.requires("openssl/1.0.2u")
+        if self.options.mysql:
+            self.requires("libmysqlclient/8.0.17")
+        if self.options.postgresql:
+            self.requires("libpq/11.5")
+
     def source(self):
         sha256 = "1c4322a68704526ca3d1f213e7b0dcd30e067a8815be2a79b2ab1197ef70dcf7"
         tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version), sha256=sha256)
@@ -41,24 +45,24 @@ class TrantorConan(ConanFile):
 
     def _configure_cmake(self):
         cmake = CMake(self)
-        # cmake.definitions["CMAKE_MODULE_PATH"] = ""
-        # cmake.definitions["CMAKE_TOOLCHAIN_FILE"] = os.path.join(self._build_subfolder, "conan_paths.cmake")
-        # cmake.definitions["BUILD_CTL"] = False
-        cmake.configure(build_folder=self._build_subfolder)
+        cmake.definitions["HAVE_SSL"] = self.options.ssl
+        cmake.definitions["HAVE_MYSQL"] = self.options.mysql
+        cmake.definitions["HAVE_POSTGRESQL"] = self.options.postgresql
+        cmake.definitions["HAVE_EMX"] = self.options
+        cmake.configure(build_folder=self._build_subfolder, source_folder=self._source_subfolder)
         return cmake
 
     def _patch(self):
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "include(QuickfixPlatformSettings)", "include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/QuickfixPlatformSettings.cmake)")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "include(QuickfixPrebuildSetup)", "include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/QuickfixPrebuildSetup.cmake)")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "include(FindSharedPtr)", "include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/FindSharedPtr.cmake)")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "include(FindUniquePtr)", "include(${CMAKE_CURRENT_SOURCE_DIR}/cmake/FindUniquePtr.cmake)")
-        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "add_subdirectory(UnitTest++)", "") 
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src/C++/double-conversion/diy-fp.cc"), '#include "double-conversion/diy-fp.h"', '#include "diy-fp.h"') 
-        tools.replace_in_file(os.path.join(self._source_subfolder, "src/C++/double-conversion/diy-fp.h"), '#include "double-conversion/utils.h"', '#include "utils.h"') 
-        
-        
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "add_subdirectory(test)", "")
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), "add_subdirectory(examples)", "")
+        tools.replace_in_file(os.path.join(self._source_subfolder, "CMakeLists.txt"), 'message("-- Project name ${CMAKE_PROJECT_NAME}")', """
+message("-- Project name ${CMAKE_PROJECT_NAME}")
+include("${CMAKE_SOURCE_DIR}/../conanbuildinfo.cmake")
+conan_basic_setup()
+""")
+
     def build(self):
-       # self._patch()
+        self._patch()
         cmake = self._configure_cmake()
         cmake.build()
 
@@ -70,4 +74,4 @@ class TrantorConan(ConanFile):
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
         if self.settings.os == "Linux":
-            self.cpp_info.libs.append("pthread")
+            self.cpp_info.system_libs.append("pthread")
